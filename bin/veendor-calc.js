3ba63cf631a770e3407379531b4214a566c17c27
cornholio <0@mcornholio.ru>
2017-10-17 07:21:08 +0300

    Fixed git lfs fail on veendor calc

--- bin/veendor-calc.js
+++ bin/veendor-calc.js
@@ -6,13 +6,17 @@ const fsExtra = require('fs-extra');
 
 const pkgJson = require('../lib/pkgjson');
 const resolveConfig = require('../lib/resolveConfig');
+const logger = require('../lib/logger');
 
 program
     .description('Calculate and print your bundle id')
     .option('-c --config [configuration-file]')
+    .option('--debug', 'don\'t remove .veendor-debug.log')
     .parse(process.argv);
 
 let config;
+
+const daLogger = logger.setDefaultLogger(1, 3);
 
 resolveConfig(program.config)
     .then(resolvedConfig => {
@@ -22,6 +26,13 @@ resolveConfig(program.config)
     .then(pkgJson.parsePkgJson)
     .then(parsedPkgJson => {
         console.log(pkgJson.calcHash(parsedPkgJson, config.packageHash));
+
+        if (!(program.debug)) {
+            return fsExtra.remove(path.resolve(process.cwd(), '.veendor-debug.log'));
+        }
+
+        process.exit(0);
     }, error => {
-        console.error(error);
+        daLogger.error(error);
+        process.exit(1);
     });
