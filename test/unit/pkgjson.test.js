f970634a1af070fea75c8868e05d66448a168ca0
cornholio <0@mcornholio.ru>
2018-02-11 18:26:45 +0300

    Added lockfileContents to calcHash

--- test/unit/pkgjson.test.js
+++ test/unit/pkgjson.test.js
@@ -9,7 +9,6 @@ const _ = require('lodash');
 
 describe('pkgjson', function () {
     describe('#calcHash', function () {
-        const PKGJSON_PATH = 'package.json';
         const PKGJSON_CONTENTS = {
             dependencies: {
                 a: '666',
@@ -24,17 +23,24 @@ describe('pkgjson', function () {
             }
         };
 
+        const LOCKFILE_CONTENTS = 'type-detect@^4.0.0:\n' +
+            '  version "4.0.8"\n' +
+            '  resolved "https://registry.yarnpkg.com/type-detect/-/type-detect-4.0.8.tgz' +
+            '#7646fb5f18871cfbb7749e69bd39a6388eb7450c"';
+
         const FAKE_HASH = '1234567890deadbeef1234567890';
 
-        const fakeSha1 = {
-            update: sinon.spy(),
-            digest: function () {
-                return FAKE_HASH
-            }
-        };
+        let fakeSha1;
 
         beforeEach(function () {
             sinon.stub(crypto, 'createHash').callsFake(function () {
+                fakeSha1 = {
+                    update: sinon.spy(),
+                    digest: function () {
+                        return FAKE_HASH
+                    }
+                };
+
                 return fakeSha1;
             });
         });
@@ -54,7 +60,7 @@ describe('pkgjson', function () {
             const spyCall = fakeSha1.update.getCall(0);
 
             assert(fakeSha1.update.called, 'sha1.update hasn\'t been called');
-            assert.deepEqual(spyCall.args[0], JSON.stringify([
+            assert.include(spyCall.args[0], JSON.stringify([
                 'a@666',
                 'b@^228',
                 'c@1.4.88',
@@ -70,12 +76,21 @@ describe('pkgjson', function () {
             const spyCall = fakeSha1.update.getCall(0);
 
             assert(fakeSha1.update.called, 'sha1.update hasn\'t been called');
-            assert.deepEqual(spyCall.args[0], JSON.stringify([
+            assert.include(spyCall.args[0], JSON.stringify([
                 'a@666',
                 'b@^228',
                 'c@1.4.88',
                 'd@^0.0.1'
             ]));
+        });
+
+        it('should add lockfile contents to hash', function () {
+            pkgjson.calcHash(PKGJSON_CONTENTS, {lockfileContents: LOCKFILE_CONTENTS});
+
+            const spyCall = fakeSha1.update.getCall(1);
+
+            assert(fakeSha1.update.called, 'sha1.update hasn\'t been called');
+            assert.include(spyCall.args[0], LOCKFILE_CONTENTS);
         });
 
         it('should return result of SHA1 digest', function () {
