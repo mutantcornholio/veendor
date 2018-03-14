a8ea4257d8209e9d086472fac487186c3795c840
cornholio <0@mcornholio.ru>
2018-03-14 15:59:15 +0300

    Hash recalculation is disabled
    It had caused more problems then it solved

--- test/integration/testCases/localPush/testcase.sh
+++ test/integration/testCases/localPush/testcase.sh
@@ -10,12 +10,7 @@ node "$rootdir/bin/veendor.js" install --debug
 npm_ver="$(npm --version)"
 
 cd "$(pwd)/local"
-if [[ "$npm_ver" == 3* ]] ||  [[ "$npm_ver" == 4* ]]; then
-    tar -xf 7d0db335c82dfd9aa2b96dabc485b89ebaa1496f.tar.gz
-else
-    tar -xf 1722dc5c5ee28cf0bcdc5ac1da82e0608b655f88.tar.gz # package-lock.json is created during the run
-                                                            # veendor should recalculate hash and push new one
-fi
+tar -xf 7d0db335c82dfd9aa2b96dabc485b89ebaa1496f.tar.gz
 
 if [[ ! -f "node_modules/deep-object-diff/package.json" ]]; then
     echo "gitPush failed; node_modules/deep-object-diff/package.json is not there"
